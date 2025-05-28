49032580609e9d4b315eff3f6b96a4f65b627cf3
sudonya <daniilroitenberg@yandex.ru>
2025-05-28 22:44:33 +0300

    gitignore

--- frontend/src/components/navBar.vue
+++ frontend/src/components/navBar.vue
@@ -20,10 +20,10 @@ export default {
   <nav class="navbar navbar-dark" style="background-color: #052b6a; color: white">
     <div class="container-fluid">
       <a class="navbar-brand ms-5" href="/">Mb</a>
-      <div class="nav-item dropdown " style="margin-right: 200px">
+      <div class="nav-item dropdown me-5">
         <a class="nav-link dropdown-toggle" href="#" id="navDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="true">{{username}}</a>
         <ul class="dropdown-menu" aria-labelledby="navDropdown">
-          <li><a class="dropdown-item" @click="logout" href="/">logout</a></li>
+          <li class="pe-0"><a class="dropdown-item" @click="logout" href="/">logout</a></li>
         </ul>
       </div>
     </div>
