c751ed41366cf145ad2cfd76914b3fa0efe775f2
sudonya <daniilroitenberg@yandex.ru>
2025-05-29 10:07:48 +0300

    edited

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -42,8 +42,8 @@ export default {
 
 <template>
   <NavBar :username="user.login" />
-  <button class="position-absolute end-0 bottom-0 btn me-5 mb-5 text-center px-3 py-2 fs-5" @click="this.$router.push('/create-melody-battle')" style="color: black; background-color:rgba(0,255,255,0.98);">+</button>
   <MelodyBattleList :list="melodyBattles" @delete="deleteMb"/>
+  <button class="position-absolute end-0 bottom-0 btn me-5 mb-5 text-center px-3 py-2 fs-5" @click="this.$router.push('/create-melody-battle')" style="color: black; background-color:rgba(0,255,255,0.98);">+</button>
 </template>
 
 <style scoped>
